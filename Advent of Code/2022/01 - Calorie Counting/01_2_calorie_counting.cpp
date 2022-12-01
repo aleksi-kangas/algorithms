b9438974105c0ecaa1f86af69fb8ef0965327e8d
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-12-01 08:08:13 +0200

    Advent of Code | 2022 | 01 Calorie Counting | Part 2

---
+++ Advent of Code/2022/01 - Calorie Counting/01_2_calorie_counting.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+vector<int> ReadAndParseInput() {
+  vector<int> total_calories(1);
+  string line;
+  int emptyLines = 0;
+  int elf = 0;
+  while (getline(cin, line)) {
+    if (line.empty()) {
+      if (++emptyLines >= 2) {
+        break;
+      }
+      total_calories.emplace_back();
+      ++elf;
+    } else {
+      emptyLines = 0;
+      total_calories[elf] += stoi(line);
+    }
+  }
+  return total_calories;
+}
+
+int Solve() {
+  vector<int> total_calories = ReadAndParseInput();
+  sort(total_calories.begin(), total_calories.end(), greater());
+  return total_calories[0] + total_calories[1] + total_calories[2];
+}
+
+int main() {
+  auto answer = Solve();
+  cout << answer << endl;
+}
