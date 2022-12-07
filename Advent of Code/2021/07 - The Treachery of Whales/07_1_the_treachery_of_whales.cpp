df890fbde75146e0dbd1c801d9028c7962e8eacd
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-12-07 10:23:26 +0200

    Advent of Code | 2021 | 07 The Treachery of Whales | Part 1

---
+++ Advent of Code/2021/07 - The Treachery of Whales/07_1_the_treachery_of_whales.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+vector<int> ParseInput() {
+  vector<int> positions;
+  string s;
+  cin >> s;
+  stringstream ss(s);
+  for (int i; ss >> i;) {
+    positions.push_back(i);
+    if (ss.peek() == ',') {
+      ss.ignore();
+    }
+  }
+  return positions;
+}
+
+int Solve() {
+  vector<int> positions = ParseInput();
+  sort(positions.begin(), positions.end());
+  int n = static_cast<int>(positions.size());
+  int target = positions[n / 2];
+  int fuel = 0;
+  for (int position : positions) {
+    fuel += abs(target - position);
+  }
+  return fuel;
+}
+
+int main() {
+  auto answer = Solve();
+  cout << answer << endl;
+}
