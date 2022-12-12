02aff7beb3af792da931c24333e8031c081f69d5
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-12-12 09:08:28 +0200

    Advent of Code | 2021 | 12 Passage Pathing | Part 1

---
+++ Advent of Code/2021/12 - Passage Pathing/12_1_passage_pathing.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+unordered_map<string, unordered_set<string>> ParseInput() {
+  unordered_map<string, unordered_set<string>> m;
+  string s;
+  while (getline(cin, s) && !s.empty()) {
+    constexpr char delimiter = '-';
+    string begin = s.substr(0, s.find(delimiter));
+    string end = s.substr(s.find(delimiter) + 1);
+    m[begin].insert(end);
+    m[end].insert(begin);
+  }
+  return m;
+}
+
+int DFS(const string& u, unordered_map<string, unordered_set<string>>& m, const set<string>& visited) {
+  if (u == "end") return 1;
+  if (all_of(u.begin(), u.end(), [](auto c) { return islower(c); }) && visited.count(u)) return 0;
+  // Must create a copy for child calls.
+  set<string> new_visited = visited;
+  new_visited.insert(u);
+  int paths = 0;
+  for (const auto& v : m[u]) {
+    paths += DFS(v, m, new_visited);
+  }
+  return paths;
+}
+
+int Solve() {
+  unordered_map<string, unordered_set<string>> m = ParseInput();
+  set<string> visited;
+  const int paths = DFS("start", m, visited);
+  return paths;
+}
+
+int main() {
+  auto answer = Solve();
+  cout << answer << endl;
+}
