05334d36850b54a1813e940990a259b4377d3915
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-25 23:23:06 +0300

    Advent of Code | 2021 | 02 Dive | Part 1

--- Advent of Code/2021/02 - Dive/02_1_dive.cpp
+++ Advent of Code/2021/02 - Dive/02_1_dive.cpp
@@ -1,80 +1,53 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <charconv>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-using namespace std;
-
-using ll = long long;
 
 enum class Direction { Forward, Down, Up };
 
 struct Command {
-  Direction direction = Direction::Forward;
-  int value = 0;
+  Direction direction{Direction::Forward};
+  std::int32_t value{0};
 };
 
-optional<Command> ParseCommand(const string& s) {
-  auto split = s.find(' ');
-  if (split == string::npos) {
-    return nullopt;
-  }
-  Command command;
-  string direction = s.substr(0, split);
-  if (direction == "down") {
+Command ParseCommand(std::string_view s) {
+  const auto split = s.find(' ');
+  if (split == std::string_view::npos)
+    throw std::invalid_argument{"Invalid command"};
+  Command command{};
+  const std::string_view direction = s.substr(0, split);
+  if (direction == "down")
     command.direction = Direction::Down;
-  } else if (direction == "up") {
+  else if (direction == "up")
     command.direction = Direction::Up;
-  }
-  string value = s.substr(split);
-  command.value = stoi(value);
+  else if (direction == "forward")
+    command.direction = Direction::Forward;
+  else
+    throw std::invalid_argument{"Invalid direction"};
+  const auto [_, ec] = std::from_chars(s.data() + split + 1, s.data() + s.size(),
+                                       command.value);
+  if (ec != std::errc{})
+    throw std::invalid_argument{"Invalid value"};
   return command;
 }
 
-int Solve() {
-  int depth = 0;
-  int x = 0;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    optional<Command> command = ParseCommand(s);
-    if (!command.has_value()) {
-      return x * depth;
-    }
-    switch (command->direction) {
+int main() {
+  std::int32_t x{0};
+  std::int32_t depth{0};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    const auto command = ParseCommand(line);
+    switch (command.direction) {
       case Direction::Forward:
-        x += command->value;
+        x += command.value;
         break;
       case Direction::Down:
-        depth += command->value;
+        depth += command.value;
         break;
       case Direction::Up:
-        depth -= command->value;
+        depth -= command.value;
         break;
     }
   }
-  return x * depth;
-}
-
-int main() {
-  int multiplication = Solve();
-  cout << multiplication << endl;
+  const std::int32_t multiplication = x * depth;
+  std::cout << multiplication << std::endl;
 }
