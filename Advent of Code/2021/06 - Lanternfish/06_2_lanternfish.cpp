d2b95b239c7553b4e7344200d7ccf6036d74822f
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-26 15:53:17 +0300

    Advent of Code | 2021 | 06 Lanternfish | Part 2

--- Advent of Code/2021/06 - Lanternfish/06_2_lanternfish.cpp
+++ Advent of Code/2021/06 - Lanternfish/06_2_lanternfish.cpp
@@ -1,76 +1,40 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <array>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
 #include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-struct Fish {
-  Fish() : timer(8) {}
-  explicit Fish(int timer) : timer(timer) {}
-  int timer;
-};
-
-vector<Fish> ParseInput() {
-  string s;
-  cin >> s;
-  vector<Fish> fish;
-  stringstream ss(s);
-  for (int i; ss >> i;) {
-    fish.emplace_back(i);
+std::vector<std::int32_t> ReadFishTimers() {
+  std::vector<std::int32_t> fish_timers{};
+  std::string line{};
+  std::cin >> line;
+  std::stringstream ss{line};
+  for (std::int32_t fish_timer; ss >> fish_timer;) {
+    fish_timers.emplace_back(fish_timer);
     if (ss.peek() == ',') {
       ss.ignore();
     }
   }
-  return fish;
-}
-
-ll Solve() {
-  vector<Fish> fish = ParseInput();
-  unordered_map<int, ll> fish_timers;
-  for (int i = 0; i < 9; ++i) {
-    fish_timers[i] = 0;
-  }
-  for (const auto& f : fish) {
-    ++fish_timers[f.timer];
-  }
-  for (int day = 1; day <= 256; ++day) {
-    const ll new_fish = fish_timers[0];
-    for (int i = 0; i < 8; ++i) {
-      fish_timers[i] = fish_timers[i + 1];
-    }
-    fish_timers[6] = fish_timers[6] + new_fish;
-    fish_timers[8] = new_fish;
-  }
-  ll sum = 0;
-  for (int i = 0; i < 9; ++i) {
-    sum += fish_timers[i];
-  }
-  return sum;
+  return fish_timers;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  const auto initial_timers = ReadFishTimers();
+  std::array<std::int64_t, 9> timers{};
+  for (int fish_timer : initial_timers) {
+    ++timers[fish_timer];
+  }
+  for (std::size_t d = 1; d <= 256; ++d) {
+    const std::int64_t new_fish_count = timers[0];
+    for (std::size_t i = 0; i < timers.size() - 1; ++i) {
+      timers[i] = timers[i + 1];
+    }
+    timers[6] += new_fish_count;
+    timers[8] = new_fish_count;
+  }
+  std::int64_t answer{0};
+  for (std::int64_t timer : timers) {
+    answer += timer;
+  }
+  std::cout << answer << std::endl;
 }
