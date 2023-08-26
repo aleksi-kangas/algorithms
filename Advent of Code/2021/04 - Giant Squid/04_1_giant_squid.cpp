456735496b27fc0e59b0e0a125d48354ef38f02b
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-26 12:13:02 +0300

    Advent of Code | 2021 | 04 Giant Squid | Part 1

--- Advent of Code/2021/04 - Giant Squid/04_1_giant_squid.cpp
+++ Advent of Code/2021/04 - Giant Squid/04_1_giant_squid.cpp
@@ -1,142 +1,100 @@
 #include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <array>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
 #include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
+#include <ranges>
 #include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
 #include <unordered_set>
-#include <utility>
 #include <vector>
-
-using namespace std;
-
-using ll = long long;
 
 class Board {
  public:
-  Board() = default;
+  static constexpr std::int32_t kSize{5};
+
+  static Board ParseBoard() {
+    Board board{};
+    std::string line{};
+    std::size_t row_index{0};
+    while (std::getline(std::cin, line) && !line.empty()) {
+      std::stringstream ss{line};
+      std::size_t column_index{0};
+      for (std::int32_t draw; ss >> draw;) {
+        board.rows_[row_index].insert(draw);
+        board.columns_[column_index++].insert(draw);
+        if (ss.peek() == ' ') {
+          ss.ignore();
+        }
+      }
+      ++row_index;
+    }
+    return board;
+  }
+
+  void MarkDraw(std::int32_t draw) {
+    std::ranges::for_each(rows_, [&](auto& row) { row.erase(draw); });
+    std::ranges::for_each(columns_, [&](auto& column) { column.erase(draw); });
+  }
 
   [[nodiscard]] bool CheckWin() const {
-    bool row_win = std::any_of(rows_.begin(), rows_.end(), [](const auto& row) { return row.empty(); });
-    bool column_win = std::any_of(columns_.begin(), columns_.end(), [](const auto& column) { return column.empty(); });
-    return row_win || column_win;
+    return std::ranges::any_of(rows_, [](const auto& row) { return row.empty(); }) ||
+           std::ranges::any_of(columns_, [](const auto& column) { return column.empty(); });
   }
 
-  [[nodiscard]] int ComputeScore() const {
-    int score = 0;
-    for (auto& row : rows_) {
-      for (int n : row) {
-        score += n;
-      }
-    }
-    return score;
-  }
-
-  void MarkNumber(int number) {
-    for (auto& row : rows_) {
-      row.erase(number);
-    }
-    for (auto& column : columns_) {
-      column.erase(number);
-    }
-  }
-
-  void ParseRow(const string& line) {
-    vector<int> numbers;
-    stringstream ss(line);
-    for (int i; ss >> i;) {
-      numbers.push_back(i);
-      if (ss.peek() == ' ') {
-        ss.ignore();
-      }
-    }
-    set<int> row;
-    for (int i = 0; i < 5; ++i) {
-      row.insert(numbers[i]);
-      if (columns_.size() == i) {
-        columns_.emplace_back();
-      }
-      columns_[i].insert(numbers[i]);
-    }
-    rows_.push_back(move(row));
+  [[nodiscard]] std::int32_t ComputeScore() const {
+    std::int32_t sum{0};
+    std::ranges::for_each(rows_, [&](const auto& row) { sum += std::accumulate(row.begin(), row.end(), 0); });
+    return sum;
   }
 
  private:
-  vector<set<int>> rows_;
-  vector<set<int>> columns_;
+  std::array<std::unordered_set<std::int32_t>, kSize> rows_{};
+  std::array<std::unordered_set<std::int32_t>, kSize> columns_{};
 };
 
-vector<int> ParseNumbers() {
-  string s;
-  cin >> s;
-  vector<int> numbers;
-
-  stringstream ss(s);
-  for (int i; ss >> i;) {
-    numbers.push_back(i);
+std::vector<std::int32_t> ReadDraws() {
+  std::vector<std::int32_t> draws{};
+  std::string draw_line{};
+  std::getline(std::cin, draw_line);
+  std::stringstream ss{draw_line};
+  for (std::int32_t draw; ss >> draw;) {
+    draws.emplace_back(draw);
     if (ss.peek() == ',') {
       ss.ignore();
     }
   }
-  cin.ignore();
-  return numbers;
+  return draws;
 }
 
-vector<Board> ParseBoards() {
-  vector<Board> boards;
-  string s;
-  int successive_empty_lines = 0;
-  while (getline(cin, s)) {
-    if (s.empty()) {
-      ++successive_empty_lines;
-      if (successive_empty_lines == 2) {
-        break;
-      }
+std::vector<Board> ReadBoards() {
+  std::int32_t empty_line_count{0};
+  std::string board_line{};
+  std::vector<Board> boards{};
+  while (empty_line_count < 2) {
+    if (std::cin.peek() == '\n') {
+      std::cin.ignore();
+      ++empty_line_count;
       continue;
     } else {
-      successive_empty_lines = 0;
-      boards.emplace_back();
-      Board& board = boards.back();
-      board.ParseRow(s);
-      for (int i = 0; i < 4; ++i) {  // Read rest of the rows of the board.
-        getline(cin, s);
-        board.ParseRow(s);
-      }
+      empty_line_count = 0;
+      boards.emplace_back(Board::ParseBoard());
     }
   }
   return boards;
 }
 
-int Solve() {
-  vector<int> numbers = ParseNumbers();
-  vector<Board> boards = ParseBoards();
-
-  for (int number : numbers) {
+int main() {
+  const auto draws = ReadDraws();
+  auto boards = ReadBoards();
+  for (const auto& draw : draws) {
     for (auto& board : boards) {
-      board.MarkNumber(number);
+      board.MarkDraw(draw);
       if (board.CheckWin()) {
-        return board.ComputeScore() * number;
+        std::cout << board.ComputeScore() * draw << std::endl;
+        return 0;
       }
     }
   }
-  return 0;
-}
-
-int main() {
-  int score = Solve();
-  cout << score << endl;
+  return -1;
 }
