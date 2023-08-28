f65d6e19fed636c0d8b4c20433d694fff652cfa8
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-28 21:47:24 +0300

    Advent of Code | 2021 | 11 Dumbo Octopus | Part 1

--- Advent of Code/2021/11 - Dumbo Octopus/11_1_dumbo_octopus.cpp
+++ Advent of Code/2021/11 - Dumbo Octopus/11_1_dumbo_octopus.cpp
@@ -1,102 +1,80 @@
 #include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <ranges>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<vector<int>> ParseInput() {
-  vector<vector<int>> m;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    vector<int> row;
-    for (char c : s) {
-      row.push_back(c - '0');
+std::vector<std::vector<std::int32_t>> ReadEnergies() {
+  std::vector<std::vector<std::int32_t>> energies{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    energies.emplace_back();
+    energies.back().reserve(line.size());
+    for (const char c : line) {
+      energies.back().emplace_back(c - '0');
     }
-    m.push_back(move(row));
   }
-  return m;
+  return energies;
 }
 
-void Flash(vector<vector<int>>& e, vector<vector<bool>>& flashed, int y, int x) {
-  int n = static_cast<int>(e.size());
-  int m = static_cast<int>(e[0].size());
-  for (int i = y - 1; i <= y + 1; ++i) {
-    for (int j = x - 1; j <= x + 1; ++j) {
-      if ((i == y && j == x) || i < 0 || j < 0 || i == n || j == m) {
-        continue;
-      }
-      ++e[i][j];
-    }
-  }
+void IncrementAllEnergies(std::vector<std::vector<std::int32_t>>& energies) {
+  std::ranges::for_each(energies, [](auto& row) { std::ranges::for_each(row, [](auto& energy) { ++energy; }); });
+}
 
-  for (int i = y - 1; i <= y + 1; ++i) {
-    for (int j = x - 1; j <= x + 1; ++j) {
-      if ((i == y && j == x) || i < 0 || j < 0 || i == n || j == m) {
+void IncrementNeighbors(std::vector<std::vector<std::int32_t>>& energies, std::int32_t i, std::int32_t j) {
+  for (std::int32_t k = i - 1; k <= i + 1; ++k) {
+    for (std::int32_t l = j - 1; l <= j + 1; ++l) {
+      if (k < 0 || static_cast<std::int32_t>(energies.size()) <= k || l < 0 ||
+          static_cast<std::int32_t>(energies[k].size()) <= l || (k == i && l == j))
         continue;
-      }
-      if (e[i][j] > 9 && !flashed[i][j]) {
-        flashed[i][j] = true;
-        Flash(e, flashed, i, j);
-      }
+      ++energies[k][l];
     }
   }
 }
 
-int Solve() {
-  int flashes = 0;
-  vector<vector<int>> e = ParseInput();
-  int n = static_cast<int>(e.size());
-  int m = static_cast<int>(e[0].size());
-  for (int s = 1; s <= 100; ++s) {
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < m; ++j) {
-        ++e[i][j];
-      }
+std::int32_t Flash(std::vector<std::vector<std::int32_t>>& energies, std::vector<std::vector<bool>>& flashed,
+                   std::int32_t i, std::int32_t j) {
+  if (flashed[i][j] || energies[i][j] <= 9)
+    return 0;
+  flashed[i][j] = true;
+  std::int32_t neighbors_flashed{0};
+  IncrementNeighbors(energies, static_cast<std::int32_t>(i), static_cast<std::int32_t>(j));
+  for (std::int32_t k = i - 1; k <= i + 1; ++k) {
+    for (std::int32_t l = j - 1; l <= j + 1; ++l) {
+      if (k < 0 || static_cast<std::int32_t>(energies.size()) <= k || l < 0 ||
+          static_cast<std::int32_t>(energies[k].size()) <= l || (k == i && l == j))
+        continue;
+      neighbors_flashed += Flash(energies, flashed, k, l);
     }
-    vector<vector<bool>> flashed(n, vector<bool>(m, false));
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < m; ++j) {
-        if (e[i][j] > 9 && !flashed[i][j]) {
-          flashed[i][j] = true;
-          Flash(e, flashed, i, j);
-        }
-      }
+  }
+  return 1 + neighbors_flashed;
+}
+
+std::int32_t FlashAll(std::vector<std::vector<std::int32_t>>& energies) {
+  std::int32_t total_flashes{0};
+  std::vector<std::vector<bool>> flashed(energies.size(), std::vector<bool>(energies[0].size(), false));
+  for (std::size_t i = 0; i < energies.size(); ++i) {
+    for (std::size_t j = 0; j < energies[i].size(); ++j) {
+      total_flashes += Flash(energies, flashed, static_cast<std::int32_t>(i), static_cast<std::int32_t>(j));
     }
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < m; ++j) {
-        if (flashed[i][j]) {
-          ++flashes;
-          e[i][j] = 0;
-        }
+  }
+  for (std::size_t i = 0; i < energies.size(); ++i) {
+    for (std::size_t j = 0; j < energies[i].size(); ++j) {
+      if (flashed[i][j]) {
+        energies[i][j] = 0;
       }
     }
   }
-  return flashes;
+  return total_flashes;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  auto energies = ReadEnergies();
+  std::int32_t total_flashes{0};
+  for (std::int32_t step = 1; step <= 100; ++step) {
+    IncrementAllEnergies(energies);
+    total_flashes += FlashAll(energies);
+  }
+  std::cout << total_flashes << '\n';
 }
