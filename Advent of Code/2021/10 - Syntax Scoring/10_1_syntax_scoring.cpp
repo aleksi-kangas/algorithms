e6601130269335ad1534e29f87d5271710801099
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-28 13:50:27 +0300

    Advent of Code | 2021 | 10 Syntax Scoring | Part 1

--- Advent of Code/2021/10 - Syntax Scoring/10_1_syntax_scoring.cpp
+++ Advent of Code/2021/10 - Syntax Scoring/10_1_syntax_scoring.cpp
@@ -1,76 +1,55 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
 #include <stack>
+#include <stdexcept>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<string> ParseInput() {
-  vector<string> lines;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    lines.push_back(move(s));
+std::int32_t Penalty(char close, char open) {
+  switch (close) {
+    case ')':
+      return open == '(' ? 0 : 3;
+    case ']':
+      return open == '[' ? 0 : 57;
+    case '}':
+      return open == '{' ? 0 : 1197;
+    case '>':
+      return open == '<' ? 0 : 25137;
+    default:
+      throw std::runtime_error{"Invalid character"};
   }
-  return lines;
-}
-
-int Solve() {
-  vector<string> lines = ParseInput();
-  int answer = 0;
-  for (const auto& line : lines) {
-    stack<char> s;
-    for (char c : line) {
-      bool is_opening = c == '(' || c == '[' || c == '{' || c == '<';
-      if (is_opening) s.push(c);
-      else {
-        if (s.empty()) {
-          if (c == ')') answer += 3;
-          else if (c == ']') answer += 57;
-          else if (c == '}') answer += 1197;
-          else if (c == '>') answer += 25137;
-          break;
-        }
-        char opening = s.top();
-        bool is_matching = (c == ')' && opening == '(') ||
-            (c == ']' && opening == '[') ||
-            (c == '}' && opening == '{') ||
-            (c == '>' && opening == '<');
-        if (is_matching) s.pop();
-        else {
-          if (c == ')') answer += 3;
-          else if (c == ']') answer += 57;
-          else if (c == '}') answer += 1197;
-          else if (c == '>') answer += 25137;
-          break;
-        }
-      }
-    }
-  }
-  return answer;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::int64_t total_penalty{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    std::stack<char> s{};
+    bool valid{true};
+    for (const char c : line) {
+      if (!valid)
+        break;
+      switch (c) {
+        case '(':
+        case '[':
+        case '{':
+        case '<': {
+          s.push(c);
+        } break;
+        case ')':
+        case ']':
+        case '}':
+        case '>': {
+          const char open = s.top();
+          s.pop();
+          const auto penalty = Penalty(c, open);
+          total_penalty += penalty;
+          if (penalty > 0) {
+            valid = false;
+          }
+        } break;
+        default:
+          throw std::runtime_error{"Invalid character"};
+      }
+    }
+  }
+  std::cout << total_penalty << '\n';
 }
