a6dc8e7ab63586d794465f93e6f9dffa08540558
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-28 13:34:43 +0300

    Advent of Code | 2021 | 09 Smoke Basin | Part 2

--- Advent of Code/2021/09 - Smoke Basin/09_2_smoke_basin.cpp
+++ Advent of Code/2021/09 - Smoke Basin/09_2_smoke_basin.cpp
@@ -1,100 +1,92 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<vector<int>> ParseInput() {
-  vector<vector<int>> m;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    vector<int> row;
-    row.reserve(s.size());
-    for (char c : s) {
-      row.push_back(c - '0');
+std::vector<std::pair<std::int32_t, std::int32_t>> SeekLowPoints(const std::vector<std::vector<std::int32_t>>& m) {
+  std::vector<std::pair<std::int32_t, std::int32_t>> low_points{};
+  for (std::size_t i = 0; i < m.size(); ++i) {
+    for (std::size_t j = 0; j < m[i].size(); ++j) {
+      const bool lower_than_above = i == 0 || m[i][j] < m[i - 1][j];
+      const bool lower_than_below = i == m.size() - 1 || m[i][j] < m[i + 1][j];
+      const bool lower_than_left = j == 0 || m[i][j] < m[i][j - 1];
+      const bool lower_than_right = j == m[i].size() - 1 || m[i][j] < m[i][j + 1];
+      if (lower_than_above && lower_than_below && lower_than_left && lower_than_right) {
+        low_points.emplace_back(i, j);
+      }
     }
-    m.push_back(move(row));
   }
-  return m;
+  return low_points;
 }
 
-int Solve() {
-  vector<vector<int>> heights = ParseInput();
-  int n = static_cast<int>(heights.size());
-  int m = static_cast<int>(heights[0].size());
-  vector<int> basin_sizes;
-  basin_sizes.reserve(n * m);
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j) {
-      if (heights[i][j] == 9) {
-        continue;
-      }
-      vector<vector<bool>> visited(n, vector<bool>(m, false));
-      queue<pair<int, int>> q;
-      q.emplace(i, j);
-      int basin_size = 1;
-      while (!q.empty()) {
-        auto [x, y] = q.front();
-        q.pop();
-        if (x > 0 && heights[x][y] < heights[x - 1][y] && heights[x - 1][y] != 9) {
-          if (!visited[x - 1][y]) {
-            visited[x - 1][y] = true;
-            q.emplace(x - 1, y);
-            ++basin_size;
-          }
-        }
-        if (x < n - 1 && heights[x][y] < heights[x + 1][y] && heights[x + 1][y] != 9) {
-          if (!visited[x + 1][y]) {
-            visited[x + 1][y] = true;
-            q.emplace(x + 1, y);
-            ++basin_size;
-          }
-        }
-        if (y > 0 && heights[x][y] < heights[x][y - 1] && heights[x][y - 1] != 9) {
-          if (!visited[x][y - 1]) {
-            visited[x][y - 1] = true;
-            q.emplace(x, y - 1);
-            ++basin_size;
-          }
-        }
-        if (y < m - 1 && heights[x][y] < heights[x][y + 1] && heights[x][y + 1] != 9) {
-          if (!visited[x][y + 1]) {
-            visited[x][y + 1] = true;
-            q.emplace(x, y + 1);
-            ++basin_size;
-          }
-        }
-      }
-      basin_sizes.push_back(basin_size);
+std::int32_t BasinSize(const std::vector<std::vector<std::int32_t>>& m,
+                       const std::pair<std::int32_t, std::int32_t>& low_point) {
+  std::int32_t size{1};
+  // BFS
+  std::vector<std::vector<bool>> visited(m.size(), std::vector<bool>(m[0].size(), false));
+  std::queue<std::pair<std::int32_t, std::int32_t>> q{};
+  q.push(low_point);
+  visited[low_point.first][low_point.second] = true;
+
+  const auto IsVisited = [&visited](std::int32_t i, std::int32_t j) -> bool {
+    return visited[i][j];
+  };
+  const auto Is9 = [&m](std::int32_t i, std::int32_t j) -> bool {
+    return m[i][j] == 9;
+  };
+  const auto IsGreater = [&m](const std::pair<std::int32_t, std::int32_t>& target,
+                              const std::pair<std::int32_t, std::int32_t>& reference) -> bool {
+    return m[target.first][target.second] > m[reference.first][reference.second];
+  };
+
+  while (!q.empty()) {
+    const auto [i, j] = q.front();
+    q.pop();
+    if (i > 0 && !IsVisited(i - 1, j) && !Is9(i - 1, j) && IsGreater({i - 1, j}, {i, j})) {
+      visited[i - 1][j] = true;
+      q.emplace(i - 1, j);
+      ++size;
+    }
+    if (i < m.size() - 1 && !IsVisited(i + 1, j) && !Is9(i + 1, j) && IsGreater({i + 1, j}, {i, j})) {
+      visited[i + 1][j] = true;
+      q.emplace(i + 1, j);
+      ++size;
+    }
+    if (j > 0 && !IsVisited(i, j - 1) && !Is9(i, j - 1) && IsGreater({i, j - 1}, {i, j})) {
+      visited[i][j - 1] = true;
+      q.emplace(i, j - 1);
+      ++size;
+    }
+    if (j < m[i].size() - 1 && !IsVisited(i, j + 1) && !Is9(i, j + 1) && IsGreater({i, j + 1}, {i, j})) {
+      visited[i][j + 1] = true;
+      q.emplace(i, j + 1);
+      ++size;
     }
   }
-  sort(basin_sizes.begin(), basin_sizes.end(), greater<>());
-  return basin_sizes[0] * basin_sizes[1] * basin_sizes[2];
+  return size;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::vector<std::vector<std::int32_t>> m{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    m.emplace_back();
+    m.back().reserve(line.size());
+    for (const char c : line) {
+      m.back().push_back(c - '0');
+    }
+  }
+  const auto low_points = SeekLowPoints(m);
+  std::priority_queue<std::int32_t> basin_sizes{};
+  for (const auto low_point : low_points) {
+    basin_sizes.push(BasinSize(m, low_point));
+  }
+  std::int64_t answer{1};
+  for (std::int32_t i = 1; i <= 3; ++i) {
+    answer *= basin_sizes.top();
+    basin_sizes.pop();
+  }
+  std::cout << answer << std::endl;
 }
