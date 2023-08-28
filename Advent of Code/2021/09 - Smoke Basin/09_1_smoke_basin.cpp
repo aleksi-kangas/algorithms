f1647264d4661448228f8ddec4997ea4292b194b
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-28 13:09:33 +0300

    Advent of Code | 2021 | 09 Smoke Basin | Part 1

--- Advent of Code/2021/09 - Smoke Basin/09_1_smoke_basin.cpp
+++ Advent of Code/2021/09 - Smoke Basin/09_1_smoke_basin.cpp
@@ -1,74 +1,38 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<vector<int>> ParseInput() {
-  vector<vector<int>> m;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    vector<int> row;
-    row.reserve(s.size());
-    for (char c : s) {
-      row.push_back(c - '0');
-    }
-    m.push_back(move(row));
-  }
-  return m;
-}
-
-int Solve() {
-  vector<vector<int>> heights = ParseInput();
-  int n = static_cast<int>(heights.size());
-  int m = static_cast<int>(heights[0].size());
-  int answer = 0;
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j) {
-      bool ok = true;
-      if (i > 0) {
-        ok &= heights[i][j] < heights[i - 1][j];
-      }
-      if (i < n - 1) {
-        ok &= heights[i][j] < heights[i + 1][j];
-      }
-      if (j > 0) {
-        ok &= heights[i][j] < heights[i][j - 1];
-      }
-      if (j < m - 1) {
-        ok &= heights[i][j] < heights[i][j + 1];
-      }
-      if (ok) {
-        answer += heights[i][j] + 1;
+std::vector<std::int32_t> SeekLowPoints(const std::vector<std::vector<std::int32_t>>& m) {
+  std::vector<std::int32_t> low_points{};
+  for (std::size_t i = 0; i < m.size(); ++i) {
+    for (std::size_t j = 0; j < m[i].size(); ++j) {
+      const bool lower_than_above = i == 0 || m[i][j] < m[i - 1][j];
+      const bool lower_than_below = i == m.size() - 1 || m[i][j] < m[i + 1][j];
+      const bool lower_than_left = j == 0 || m[i][j] < m[i][j - 1];
+      const bool lower_than_right = j == m[i].size() - 1 || m[i][j] < m[i][j + 1];
+      if (lower_than_above && lower_than_below && lower_than_left && lower_than_right) {
+        low_points.push_back(m[i][j]);
       }
     }
   }
-  return answer;
+  return low_points;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::vector<std::vector<std::int32_t>> m{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    m.emplace_back();
+    m.back().reserve(line.size());
+    for (const char c : line) {
+      m.back().push_back(c - '0');
+    }
+  }
+  const auto low_points = SeekLowPoints(m);
+  std::int32_t sum{0};
+  for (const auto low_point : low_points) {
+    sum += low_point + 1;
+  }
+  std::cout << sum << std::endl;
 }
