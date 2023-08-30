6e21de3655c90e63b571ec5cf49101cad687c817
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-30 12:41:05 +0300

    Advent of Code | 2021 | 14 Extended Polymerization | Part 1

--- Advent of Code/2021/14 - Extended Polymerization/14_1_extended_polymerization.cpp
+++ Advent of Code/2021/14 - Extended Polymerization/14_1_extended_polymerization.cpp
@@ -1,95 +1,75 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <limits>
+#include <list>
+#include <stdexcept>
 #include <string>
-#include <tuple>
 #include <unordered_map>
-#include <unordered_set>
 #include <utility>
-#include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-struct Node {
-  char c;
-  Node* next;
+template <>
+struct std::hash<std::pair<char, char>> {
+  std::size_t operator()(const std::pair<char, char>& p) const noexcept {
+    const std::size_t h1 = std::hash<char>{}(p.first);
+    const std::size_t h2 = std::hash<char>{}(p.second);
+    return h1 ^ (h2 << 1);
+  }
 };
 
-map<pair<char, char>, char> ParseInsertionRules() {
-  map<pair<char, char>, char> insertion_rules;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    char a = s[0];
-    char b = s[1];
-    char c = s[s.size() - 1];
-    insertion_rules.emplace(make_pair(a, b), c);
+std::list<char> ReadPolymerTemplate() {
+  std::list<char> polymer_template{};
+  std::string line{};
+  std::getline(std::cin, line);
+  for (const auto c : line) {
+    polymer_template.emplace_back(c);
   }
-  return insertion_rules;
+  return polymer_template;
 }
 
-int Solve() {
-  string base;
-  cin >> base;
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  cin.get();
-  map<pair<char, char>, char> insertion_rules = ParseInsertionRules();
-
-  Node* head = nullptr;
-  Node* previous = nullptr;
-  for (char c : base) {
-    Node* node = new Node{c, nullptr};
-    if (head == nullptr) head = node;
-    if (previous != nullptr) previous->next = node;
-    previous = node;
+std::unordered_map<std::pair<char, char>, char> ReadInsertionRules() {
+  std::unordered_map<std::pair<char, char>, char> rules{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    constexpr std::string_view kArrow{" -> "};
+    const auto arrow_index = line.find(kArrow);
+    if (arrow_index == std::string::npos)
+      throw std::invalid_argument{"Invalid rule"};
+    rules[{line[0], line[1]}] = line[arrow_index + kArrow.size()];
   }
+  return rules;
+}
 
-  for (int i = 0; i < 10; ++i) {
-    Node* current = head;
-    while (current != nullptr && current->next != nullptr) {
-      const char a = current->c;
-      Node* next = current->next;
-      const char b = next->c;
-      const char c = insertion_rules[make_pair(a, b)];
-      Node* between = new Node{c, nullptr};
-      current->next = between;
-      between->next = next;
-      current = next;
+void InsertionStep(std::list<char>& polymer_template, const std::unordered_map<std::pair<char, char>, char>& rules) {
+  auto previous = polymer_template.begin();
+  for (auto current = std::next(previous); current != polymer_template.end(); ++current) {
+    if (const auto it = rules.find({*previous, *current}); it != rules.end()) {
+      polymer_template.insert(current, it->second);
     }
+    previous = current;
   }
+}
 
-  unordered_map<char, int> freq;
-  Node* current = head;
-  while (current != nullptr) {
-    ++freq[current->c];
-    current = current->next;
+std::pair<std::int32_t, std::int32_t> MinMaxFrequency(const std::list<char>& polymer_template) {
+  std::unordered_map<char, std::int32_t> frequency{};
+  for (const auto c : polymer_template) {
+    ++frequency[c];
   }
-  int maximum = 0;
-  int minimum = numeric_limits<int>::max();
-  for (const auto& [c, f] : freq) {
-    maximum = max(maximum, f);
-    minimum = min(minimum, f);
+  std::int32_t min_frequency{std::numeric_limits<std::int32_t>::max()};
+  std::int32_t max_frequency{std::numeric_limits<std::int32_t>::min()};
+  for (const auto [_, f] : frequency) {
+    min_frequency = std::min(min_frequency, f);
+    max_frequency = std::max(max_frequency, f);
   }
-
-  return maximum - minimum;
+  return {min_frequency, max_frequency};
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  auto polymer_template = ReadPolymerTemplate();
+  std::cin.ignore();
+  const auto rules = ReadInsertionRules();
+  for (std::size_t step = 1; step <= 10; ++step) {
+    InsertionStep(polymer_template, rules);
+  }
+  const auto [min_frequency, max_frequency] = MinMaxFrequency(polymer_template);
+  std::cout << max_frequency - min_frequency << '\n';
 }
