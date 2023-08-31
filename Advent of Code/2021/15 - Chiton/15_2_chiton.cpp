fe1f8da65bf4585a3e4fd6db44b79af4ee3e87c9
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-08-31 13:20:19 +0300

    Advent of Code | 2021 | 15 Chiton | Part 2

--- Advent of Code/2021/15 - Chiton/15_2_chiton.cpp
+++ Advent of Code/2021/15 - Chiton/15_2_chiton.cpp
@@ -1,101 +1,87 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <array>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
+#include <limits>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
-using namespace std;
+using position_t = std::pair<std::size_t, std::size_t>;
+using pq_element_t = std::pair<std::int32_t, position_t>;
 
-using ll = long long;
-
-using pq_elem = tuple<int, int, int>;
-
-vector<vector<int>> ReadInput() {
-  vector<vector<int>> m;
-  string s;
-  while (getline(cin, s) && !s.empty()) {
-    vector<int> row;
-    for (char c : s) {
-      row.push_back(c - '0');
+std::vector<std::vector<std::int32_t>> ReadRisks() {
+  std::vector<std::vector<std::int32_t>> risks{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    risks.emplace_back();
+    risks.back().reserve(line.size());
+    for (const char c : line) {
+      risks.back().push_back(c - '0');
     }
-    m.push_back(std::move(row));
   }
-  return m;
+  return risks;
 }
 
-vector<vector<int>> Expand(const vector<vector<int>>& risk) {
-  const int n = static_cast<int>(risk.size());
-  const int m = static_cast<int>(risk[0].size());
-  vector<vector<int>> expanded(n * 5, vector<int>(m * 5, 0));
-  for (int i = 0; i < n * 5; ++i) {
-    for (int j = 0; j < m * 5; ++j) {
-      const int original_risk = risk[i % n][j % m];
-      const int r = i / n;
-      const int c = j / m;
-      expanded[i][j] = (original_risk + r + c - 1) % 9 + 1;
+void Expand(std::vector<std::vector<std::int32_t>>& risks, std::size_t factor) {
+  const std::size_t n = risks.size();
+  const std::size_t m = risks[0].size();
+  for (auto& r : risks) {
+    r.resize(m * factor, -1);
+  }
+  risks.resize(n * factor, std::vector<std::int32_t>(m * factor, -1));
+  for (std::size_t i = 0; i < n * factor; ++i) {
+    for (std::size_t j = 0; j < m * factor; ++j) {
+      if (i < n && j < m)
+        continue;
+      const std::size_t tile_dy = i / n;
+      const std::size_t tile_dx = j / m;
+      const std::int32_t original_risk = risks[i % n][j % m];
+      risks[i][j] = (original_risk + static_cast<std::int32_t>(tile_dy) + static_cast<std::int32_t>(tile_dx) - 1) % 9 + 1;
     }
   }
-  return expanded;
 }
 
-int Solve() {
-  const vector<vector<int>> risk = Expand(ReadInput());
-  const int n = static_cast<int>(risk.size());
-  const int m = static_cast<int>(risk[0].size());
-
-  vector<vector<int>> total_risk(n, vector<int>(m, numeric_limits<int>::max()));
-  total_risk[0][0] = 0;
-
-  priority_queue<pq_elem, vector<pq_elem>, greater<>> pq;
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j) {
-      pq.emplace(total_risk[i][j], i, j);
+std::vector<std::vector<std::int32_t>> MinimumTotalRisk(const std::vector<std::vector<std::int32_t>>& risks) {
+  const std::size_t n = risks.size();
+  const std::size_t m = risks[0].size();
+  std::vector<std::vector<std::int32_t>> minimum_total_risks(
+      n, std::vector<std::int32_t>(m, std::numeric_limits<std::int32_t>::max()));
+  minimum_total_risks[0][0] = 0;
+  std::priority_queue<pq_element_t, std::vector<pq_element_t>, std::greater<>> mpq{};
+  for (std::size_t i = 0; i < n; ++i) {
+    for (std::size_t j = 0; j < m; ++j) {
+      mpq.emplace(minimum_total_risks[i][j], position_t{i, j});
     }
   }
 
-  while (!pq.empty()) {
-    const pq_elem elem = pq.top();
-    pq.pop();
-    // C++ priority queue implementation does not support 'change key' operation out of the box,
-    // so I use 'lazy-deletion' by inserting a new element with the changed key,
-    // and checking here if the element is already processed.
-    if (total_risk[get<1>(elem)][get<2>(elem)] < get<0>(elem)) continue;
-    constexpr int kDr[] = {-1, 0, 1, 0};
-    constexpr int kDc[] = {0, 1, 0, -1};
-    for (int i = 0; i < 4; ++i) {
-      const int r = get<1>(elem) + kDr[i];
-      const int c = get<2>(elem) + kDc[i];
-      if (r < 0 || c < 0 || r == n || c == m) continue;
-      const int new_total_risk = get<0>(elem) + risk[r][c];
-      if (new_total_risk < total_risk[r][c]) {
-        total_risk[r][c] = new_total_risk;
-        pq.emplace(new_total_risk, r, c);
+  while (!mpq.empty()) {
+    const auto [current_risk, current_position] = mpq.top();
+    mpq.pop();
+    // C++ Priority Queue implementation does not support the 'change key' operation out of the box.
+    // Instead, we'll always push the position with the new key, and let the old key be removed later by the following if-clause.
+    if (current_risk > minimum_total_risks[current_position.first][current_position.second])
+      continue;
+    constexpr std::array<std::int32_t, 4> kDx{-1, 0, 1, 0};
+    constexpr std::array<std::int32_t, 4> kDy{0, -1, 0, 1};
+    for (std::int32_t i = 0; i < 4; ++i) {
+      const std::int32_t y = static_cast<std::int32_t>(current_position.first) + kDy[i];
+      const std::int32_t x = static_cast<std::int32_t>(current_position.second) + kDx[i];
+      if (y < 0 || n <= y || x < 0 || m <= x)
+        continue;
+      const auto new_total_minimum_risk = current_risk + risks[y][x];
+      if (new_total_minimum_risk < minimum_total_risks[y][x]) {
+        minimum_total_risks[y][x] = new_total_minimum_risk;
+        mpq.emplace(new_total_minimum_risk, position_t{y, x});
       }
     }
   }
-
-  return total_risk[n - 1][m - 1];
+  return minimum_total_risks;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  auto risks = ReadRisks();
+  Expand(risks, 5);
+  const auto minimum_total_risks = MinimumTotalRisk(risks);
+  std::cout << minimum_total_risks.back().back() << '\n';
 }
