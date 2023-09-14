d547283225afc9d5f6b134240e0a406239d6e20f
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-09-14 13:29:38 +0300

    Advent of Code | 2022 | 01 Calorie Counting | Part 2

--- Advent of Code/2022/01 - Calorie Counting/01_2_calorie_counting.cpp
+++ Advent of Code/2022/01 - Calorie Counting/01_2_calorie_counting.cpp
@@ -1,58 +1,29 @@
 #include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<int> ReadAndParseInput() {
-  vector<int> total_calories(1);
-  string line;
-  int emptyLines = 0;
-  int elf = 0;
-  while (getline(cin, line)) {
-    if (line.empty()) {
-      if (++emptyLines >= 2) {
-        break;
-      }
-      total_calories.emplace_back();
-      ++elf;
-    } else {
-      emptyLines = 0;
-      total_calories[elf] += stoi(line);
-    }
+std::int32_t ReadElfCalories() {
+  std::int32_t calories{0};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    const std::int32_t c = std::stoi(line);
+    calories += c;
   }
-  return total_calories;
-}
-
-int Solve() {
-  vector<int> total_calories = ReadAndParseInput();
-  sort(total_calories.begin(), total_calories.end(), greater());
-  return total_calories[0] + total_calories[1] + total_calories[2];
+  return calories;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::priority_queue<std::int32_t> elves{};
+  while (std::cin.peek() != '\n') {
+    const std::int32_t elf_calories = ReadElfCalories();
+    elves.push(elf_calories);
+  }
+  std::int32_t answer{0};
+  for (std::size_t i = 1; i <= 3; ++i) {
+    answer += elves.top();
+    elves.pop();
+  }
+  std::cout << answer << std::endl;
 }
