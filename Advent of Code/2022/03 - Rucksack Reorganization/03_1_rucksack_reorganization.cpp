09012037c66d111a4bf403358ec587c316ead098
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-09-14 15:22:17 +0300

    Advent of Code | 2022 | 03 Rucksack Reorganization | Part 1

--- Advent of Code/2022/03 - Rucksack Reorganization/03_1_rucksack_reorganization.cpp
+++ Advent of Code/2022/03 - Rucksack Reorganization/03_1_rucksack_reorganization.cpp
@@ -1,67 +1,35 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <stdexcept>
 #include <string>
-#include <tuple>
-#include <unordered_map>
 #include <unordered_set>
-#include <utility>
-#include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-using Compartment = unordered_set<char>;
-
-vector<pair<Compartment, Compartment>> ReadRucksacks() {
-  vector<pair<Compartment, Compartment>> rucksacks;
-  string r;
-  while (getline(cin, r) && !r.empty()) {
-    const size_t m = r.size() / 2;
-    pair<Compartment, Compartment> compartments{};
-    for (size_t i = 0; i < r.size(); ++i) {
-      if (i < m) compartments.first.insert(r[i]);
-      else compartments.second.insert(r[i]);
-    }
-    rucksacks.push_back(std::move(compartments));
+std::int32_t CommonItemPriority(std::string_view rucksack) {
+  if (rucksack.size() % 2 != 0)
+    throw std::invalid_argument{"Compartments must be of the same size"};
+  const std::size_t compartment_size = rucksack.size() / 2;
+  std::unordered_set<char> first_compartment{};
+  for (std::size_t i = 0; i < compartment_size; ++i) {
+    first_compartment.insert(rucksack[i]);
   }
-  return rucksacks;
-}
-
-int Solve() {
-  const auto rucksacks = ReadRucksacks();
-  int priority_sum = 0;
-  for (const auto& [compartment1, compartment2] : rucksacks) {
-    for (char c = 'a'; c <= 'z'; ++c) {
-      if (compartment1.contains(c) && compartment2.contains(c)) {
-        priority_sum += c - 'a' + 1;
-      }
-    }
-    for (char c = 'A'; c <= 'Z'; ++c) {
-      if (compartment1.contains(c) && compartment2.contains(c)) {
-        priority_sum += c - 'A' + 27;
-      }
-    }
+  for (std::size_t i = 0; i < compartment_size; ++i) {
+    if (!first_compartment.contains(rucksack[compartment_size + i]))
+      continue;
+    const char c = rucksack[compartment_size + i];
+    if (c - 'a' >= 0 && c - 'a' <= 26)
+      return c - 'a' + 1;
+    if (c - 'A' >= 0 && c - 'A' <= 26)
+      return c - 'A' + 27;
+    throw std::invalid_argument{"Expected a alphabetic character"};
   }
-  return priority_sum;
+  throw std::invalid_argument{"Expected one common item to exist"};
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::int32_t priority_sum{0};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    priority_sum += CommonItemPriority(line);
+  }
+  std::cout << priority_sum << std::endl;
 }
