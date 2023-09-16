6dd4ca4b98c686d7053183557c1b5597b9654b43
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-09-16 12:13:22 +0300

    Advent of Code | 2022 | 07 No Space Left On Device | Part 2

--- Advent of Code/2022/07 - No Space Left On Device/07_2_no_space_left_on_device.cpp
+++ Advent of Code/2022/07 - No Space Left On Device/07_2_no_space_left_on_device.cpp
@@ -1,129 +1,130 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
+#include <limits>
 #include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <stdexcept>
 #include <string>
-#include <tuple>
 #include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
-using namespace std;
-
-using ll = long long;
+enum class Command { kNone, kCD, kLS };
 
 struct File {
-  string name;
-  int size;
+  std::string name{};
+  std::uint64_t size{};
+
+  File(std::string name, std::uint64_t size) : name{std::move(name)}, size{size} {}
 };
 
 struct Directory {
-  string name;
-  shared_ptr<Directory> parent;
-  unordered_map<string, shared_ptr<Directory>> children;
-  std::vector<File> files;
+  std::string name{};
+  Directory* parent{nullptr};
+  std::unordered_map<std::string, std::unique_ptr<Directory>> subdirectories{};
+  std::vector<File> files{};
+
+  Directory(std::string name, Directory* parent) : name{std::move(name)}, parent{parent} {}
+
+  [[nodiscard]] std::uint64_t Size() const {
+    std::uint64_t size{0};
+    for (const auto& [_, subdirectory] : subdirectories) {
+      size += subdirectory->Size();
+    }
+    for (const auto& file : files) {
+      size += file.size;
+    }
+    return size;
+  }
 };
 
-vector<string> Split(const string &s, char c) {
-  vector<string> result;
-  stringstream ss(s);
-  string item;
-  while (getline(ss, item, c)) {
-    result.push_back(item);
+std::vector<std::string_view> Split(std::string_view s, char c) {
+  std::vector<std::string_view> result{};
+  std::size_t start = 0;
+  std::size_t end = s.find(c);
+  while (end != std::string_view::npos) {
+    result.push_back(s.substr(start, end - start));
+    start = end + 1;
+    end = s.find(c, start);
   }
+  result.push_back(s.substr(start, end));
   return result;
 }
 
-bool IsCommand(const string &s) { return s[0] == '$'; }
+bool IsCommand(std::string_view s) {
+  return s.starts_with("$");
+}
 
-void BuildFilesystemTree(shared_ptr<Directory> root,
-                         const vector<string> &lines) {
-  auto current = root;
-  for (const auto &line : lines) {
-    vector<string> parts = Split(line, ' ');
-    if (IsCommand(parts[0])) {
-      if (parts[1] == "cd") {
-        if (parts[2] == "..") {
-          current = current->parent;
-        } else if (parts[2] == "/") {
-          current = root;
+Command ParseCommand(std::string_view line) {
+  const auto line_parts = Split(line, ' ');
+  if (line_parts[1] == "cd") {
+    return Command::kCD;
+  } else if (line_parts[1] == "ls") {
+    return Command::kLS;
+  }
+  return Command::kNone;
+}
+
+std::unique_ptr<Directory> ParseFilesystemTree() {
+  std::unique_ptr<Directory> root{nullptr};
+  std::string line{};
+  Directory* current{nullptr};
+  Command command{Command::kNone};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    const auto line_parts = Split(line, ' ');
+    if (IsCommand(line)) {
+      if (line_parts[1] == "cd") {
+        if (line_parts[2] == "/") {
+          if (root == nullptr) {
+            root = std::make_unique<Directory>("/", nullptr);
+            current = root.get();
+          } else {
+            current = root.get();
+          }
+        } else if (line_parts[2] == "..") {
+          if (current->parent != nullptr) {
+            current = current->parent;
+          }
         } else {
-          current = current->children[parts[2]];
+          current = current->subdirectories[std::string{line_parts[2]}].get();
         }
-      } else if (parts[1] == "ls") {
-        continue;
-      } else {
-        throw runtime_error{"Unknown command: " + parts[1]};
       }
     } else {
-      if (parts[0] == "dir") {
-        auto child = make_shared<Directory>();
-        child->parent = current;
-        child->name = parts[1];
-        current->children[child->name] = child;
+      if (line_parts.size() != 2)
+        throw std::runtime_error{"Invalid line"};
+      if (line_parts[0] == "dir") {
+        current->subdirectories[std::string{line_parts[1]}] =
+            std::make_unique<Directory>(std::string{line_parts[1]}, current);
       } else {
-        File file{.name = parts[1], .size = stoi(parts[0])};
-        current->files.push_back(file);
+        current->files.emplace_back(std::string{line_parts[1]}, std::stoull(std::string{line_parts[0]}));
       }
     }
   }
+  return root;
 }
 
-ll DirectorySize(shared_ptr<Directory> directory) {
-  ll sum = 0;
-  for (const auto &file : directory->files) {
-    sum += file.size;
+std::uint64_t FindSmallestDirectoryToRemove(const Directory* root, std::uint64_t size_required) {
+  std::uint64_t smallest_size{std::numeric_limits<std::uint64_t>::max()};
+  for (const auto& [_, subdirectory] : root->subdirectories) {
+    const std::uint64_t subdirectory_size = subdirectory->Size();
+    if (subdirectory_size >= size_required) {
+      smallest_size = std::min(smallest_size, subdirectory_size);
+    }
+    smallest_size = std::min(smallest_size, FindSmallestDirectoryToRemove(subdirectory.get(), size_required));
   }
-  for (const auto &[_, child] : directory->children) {
-    sum += DirectorySize(child);
-  }
-  return sum;
-}
-
-void WalkTree(shared_ptr<Directory> directory, ll to_free, ll &minimum) {
-  ll directory_size = DirectorySize(directory);
-  if (directory_size >= to_free) {
-    minimum = min(minimum, directory_size);
-  }
-  for (const auto &[_, child] : directory->children) {
-    WalkTree(child, to_free, minimum);
-  }
-}
-
-ll Solve() {
-  vector<string> lines;
-  string line;
-  while (getline(cin, line) && !line.empty()) {
-    lines.push_back(line);
-  }
-  auto root = make_shared<Directory>();
-  BuildFilesystemTree(root, lines);
-  ll in_use = DirectorySize(root);
-  constexpr ll kTotal = 70000000;
-  ll free = kTotal - in_use;
-  constexpr ll kMinimum = 30000000;
-  ll to_free = kMinimum - free;
-
-  ll minimum = numeric_limits<ll>::max();
-  WalkTree(root, to_free, minimum);
-  return minimum;
+  return smallest_size;
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  constexpr std::uint64_t kFilesystemSize{70000000};
+  constexpr std::uint64_t kSizeRequired{30000000};
+  const auto root = ParseFilesystemTree();
+  const std::uint64_t disk_usage = root->Size();
+  const std::uint64_t unused_space = kFilesystemSize - disk_usage;
+  if (unused_space >= kSizeRequired) {
+    std::cout << 0 << std::endl;
+    return 0;
+  }
+  const std::uint64_t size_required = kSizeRequired - unused_space;
+  const std::uint64_t answer = FindSmallestDirectoryToRemove(root.get(), size_required);
+  std::cout << answer << std::endl;
 }
