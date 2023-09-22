ee0d8ce976a770646de6aad5e3ab96b629693b71
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-09-22 08:23:06 +0300

    Advent of Code | 2022 | 08 Treetop Tree House | Part 1

--- Advent of Code/2022/08 - Treetop Tree House/08_1_treetop_tree_house.cpp
+++ Advent of Code/2022/08 - Treetop Tree House/08_1_treetop_tree_house.cpp
@@ -1,115 +1,71 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 
-using namespace std;
-
-using ll = long long;
-
-vector<vector<int>> ReadInput() {
-  vector<vector<int>> grid;
-  string line;
-  while (getline(cin, line) && !line.empty()) {
-    vector<int> row;
-    for (char c : line) {
+std::vector<std::vector<std::int32_t>> ReadGrid() {
+  std::vector<std::vector<std::int32_t>> grid{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    std::vector<std::int32_t> row{};
+    for (const char c : line) {
       row.push_back(c - '0');
     }
-    grid.push_back(row);
+    grid.push_back(std::move(row));
   }
   return grid;
 }
 
-bool IsVisibleLeft(const vector<vector<int>> &grid, int r, int c) {
-  for (int j = c - 1; j >= 0; --j) {
-    if (grid[r][c] <= grid[r][j]) {
+bool IsVisibleLeft(const std::vector<std::vector<std::int32_t>>& grid, std::size_t i, std::size_t j) {
+  for (std::int32_t k = static_cast<std::int32_t>(j) - 1; k >= 0; --k) {
+    if (grid[i][j] <= grid[i][k])
       return false;
-    }
   }
   return true;
 }
 
-bool IsVisibleRight(const vector<vector<int>> &grid, int r, int c) {
-  for (int j = c + 1; j < grid[r].size(); ++j) {
-    if (grid[r][c] <= grid[r][j]) {
+bool IsVisibleRight(const std::vector<std::vector<std::int32_t>>& grid, std::size_t i, std::size_t j) {
+  for (std::size_t k = j + 1; k < grid[0].size(); ++k) {
+    if (grid[i][j] <= grid[i][k])
       return false;
-    }
   }
   return true;
 }
 
-bool IsVisibleUp(const vector<vector<int>> &grid, int r, int c) {
-  for (int i = r - 1; i >= 0; --i) {
-    if (grid[r][c] <= grid[i][c]) {
+bool IsVisibleUp(const std::vector<std::vector<std::int32_t>>& grid, std::size_t i, std::size_t j) {
+  for (std::int32_t k = static_cast<std::int32_t>(i) - 1; k >= 0; --k) {
+    if (grid[i][j] <= grid[k][j])
       return false;
-    }
   }
   return true;
 }
 
-bool IsVisibleDown(const vector<vector<int>> &grid, int r, int c) {
-  for (int i = r + 1; i < grid.size(); ++i) {
-    if (grid[r][c] <= grid[i][c]) {
+bool IsVisibleDown(const std::vector<std::vector<std::int32_t>>& grid, std::size_t i, std::size_t j) {
+  for (std::size_t k = i + 1; k < grid.size(); ++k) {
+    if (grid[i][j] <= grid[k][j])
       return false;
-    }
   }
   return true;
 }
 
-struct Visible {
-  bool left = true;
-  bool right = true;
-  bool up = true;
-  bool down = true;
-};
-
-int Solve() {
-  const auto grid = ReadInput();
-  const int n = static_cast<int>(grid.size());
-  const int m = static_cast<int>(grid[0].size());
-
-  vector<vector<Visible>> visible(n, vector<Visible>(m));
-  for (int r = 0; r < n; ++r) {
-    for (int c = 0; c < m; ++c) {
-      visible[r][c].left = IsVisibleLeft(grid, r, c);
-      visible[r][c].right = IsVisibleRight(grid, r, c);
-      visible[r][c].up = IsVisibleUp(grid, r, c);
-      visible[r][c].down = IsVisibleDown(grid, r, c);
+int main() {
+  auto grid = ReadGrid();
+  const std::size_t n = grid.size();
+  const std::size_t m = grid[0].size();
+  std::vector<std::vector<bool>> visible(n, std::vector<bool>(m, false));
+  for (std::size_t i = 0; i < n; ++i) {
+    for (std::size_t j = 0; j < m; ++j) {
+      visible[i][j] = IsVisibleLeft(grid, i, j) || IsVisibleRight(grid, i, j) || IsVisibleUp(grid, i, j) ||
+                      IsVisibleDown(grid, i, j);
     }
   }
-
-  int answer = 0;
-  for (int r = 0; r < n; ++r) {
-    for (int c = 0; c < m; ++c) {
-      if (visible[r][c].left || visible[r][c].right || visible[r][c].up ||
-          visible[r][c].down) {
+  std::int32_t answer{0};
+  for (std::size_t i = 0; i < n; ++i) {
+    for (std::size_t j = 0; j < m; ++j) {
+      if (visible[i][j]) {
         ++answer;
       }
     }
   }
-  return answer;
-}
-
-int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::cout << answer << std::endl;
 }
