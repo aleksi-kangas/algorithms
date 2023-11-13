00e9ec54c4ae9c33456ebbaf15f343e9dfe17b0e
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-11-13 18:31:38 +0200

    Advent of Code | 2022 | 09 Rope Bridge | Part 2

--- Advent of Code/2022/09 - Rope Bridge/09_2_rope_bridge.cpp
+++ Advent of Code/2022/09 - Rope Bridge/09_2_rope_bridge.cpp
@@ -1,120 +1,100 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <array>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <stdexcept>
 #include <string>
-#include <tuple>
-#include <unordered_map>
 #include <unordered_set>
 #include <utility>
-#include <vector>
-
-using namespace std;
-
-using ll = long long;
 
 enum class Direction { kLeft, kRight, kUp, kDown };
 
-vector<pair<Direction, int>> ReadInput() {
-  vector<pair<Direction, int>> instructions;
-  string line;
-  while (getline(cin, line) && !line.empty()) {
-    char dir = line[0];
-    int dist = stoi(line.substr(1));
-    switch (dir) {
+struct Point {
+  std::int32_t x{0};
+  std::int32_t y{0};
+  auto operator<=>(const Point&) const = default;
+};
+
+using position_t = std::pair<std::int32_t, std::int32_t>;
+
+template <>
+struct std::hash<Point> {
+  std::size_t operator()(const Point& p) const noexcept {
+    const std::size_t h1 = std::hash<std::int32_t>{}(p.x);
+    const std::size_t h2 = std::hash<std::int32_t>{}(p.y);
+    return h1 ^ (h2 << 1);
+  }
+};
+
+std::pair<Direction, std::int32_t> ParseMotion(std::string_view s) {
+  Direction d{Direction::kLeft};
+  switch (s[0]) {
     case 'L':
-      instructions.emplace_back(Direction::kLeft, dist);
+      d = Direction::kLeft;
       break;
     case 'R':
-      instructions.emplace_back(Direction::kRight, dist);
+      d = Direction::kRight;
       break;
     case 'U':
-      instructions.emplace_back(Direction::kUp, dist);
+      d = Direction::kUp;
       break;
     case 'D':
-      instructions.emplace_back(Direction::kDown, dist);
+      d = Direction::kDown;
       break;
     default:
-      assert(false);
-    }
+      throw std::runtime_error{"Invalid direction"};
   }
-  return instructions;
+  const std::int32_t distance = std::stoi(std::string{s.substr(1)});
+  return {d, distance};
 }
 
-void MoveHead(pair<int, int> &head, Direction direction) {
+void MoveHead(Point& head, Direction direction) {
   switch (direction) {
-  case Direction::kLeft:
-    --head.first;
-    break;
-  case Direction::kRight:
-    ++head.first;
-    break;
-  case Direction::kUp:
-    ++head.second;
-    break;
-  case Direction::kDown:
-    --head.second;
-    break;
-  default:
-    assert(false);
+    case Direction::kLeft:
+      --head.x;
+      break;
+    case Direction::kRight:
+      ++head.x;
+      break;
+    case Direction::kUp:
+      ++head.y;
+      break;
+    case Direction::kDown:
+      --head.y;
+      break;
+    default:
+      throw std::runtime_error{"Invalid direction"};
   }
 }
 
-void MoveTail(const pair<int, int> &head, pair<int, int> &tail) {
-  int dx = head.first - tail.first;
-  int dy = head.second - tail.second;
+void MoveTail(const Point& head, Point& tail) {
+  const std::int32_t dx = head.x - tail.x;
+  const std::int32_t dy = head.y - tail.y;
   if (dx == 0 || dy == 0) {
-    if (abs(dx) >= 2) {
-      tail.first += (dx > 0) ? 1 : -1;
+    if (std::abs(dx) >= 2) {
+      tail.x += (dx > 0) ? 1 : -1;
     }
-    if (abs(dy) >= 2) {
-      tail.second += (dy > 0) ? 1 : -1;
+    if (std::abs(dy) >= 2) {
+      tail.y += (dy > 0) ? 1 : -1;
     }
-  } else if (abs(dx) != 1 || abs(dy) != 1) {
-    tail.first += (dx > 0) ? 1 : -1;
-    tail.second += (dy > 0) ? 1 : -1;
+  } else if (std::abs(dx) != 1 || std::abs(dy) != 1) {
+    tail.x += (dx > 0) ? 1 : -1;
+    tail.y += (dy > 0) ? 1 : -1;
   }
-}
-
-int Solve() {
-  vector<pair<Direction, int>> instructions = ReadInput();
-  pair<int, int> head = {0, 0};
-  vector<pair<int, int>> knots(9, {0, 0});
-  set<pair<int, int>> visited;
-  visited.insert(knots[8]);
-
-  for (const auto &[direction, distance] : instructions) {
-    for (int d = 0; d < distance; ++d) {
-      // Move the head
-      MoveHead(head, direction);
-
-      // Move the knots
-      pair<int, int> previous = head;
-      for (int k = 0; k < 9; ++k) {
-        MoveTail(previous, knots[k]);
-        previous = knots[k];
-      }
-      visited.insert(knots[8]);
-    }
-  }
-
-  return static_cast<int>(visited.size());
 }
 
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::array<Point, 10> rope{};
+  std::unordered_set<Point> tail_visited{};
+  std::string line{};
+  while (std::getline(std::cin, line) && !line.empty()) {
+    const auto [d, distance] = ParseMotion(line);
+    for (std::int32_t i = 0; i < distance; ++i) {
+      MoveHead(rope[0], d);
+      for (std::size_t j = 1; j < rope.size(); ++j) {
+        MoveTail(rope[j - 1], rope[j]);
+      }
+      tail_visited.insert(rope[rope.size() - 1]);
+    }
+  }
+  std::cout << tail_visited.size() << '\n';
 }
