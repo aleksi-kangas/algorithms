4e8b4b9112da1d9465592a1199c10ab07551c60a
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-11-24 06:17:14 +0200

    Advent of Code | 2022 | 11 Monkey in the Middle | Part 1

--- Advent of Code/2022/11 - Monkey in the Middle/11_1_monkey_in_the_middle.cpp
+++ Advent of Code/2022/11 - Monkey in the Middle/11_1_monkey_in_the_middle.cpp
@@ -1,216 +1,135 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
+#include <array>
+#include <cstdint>
 #include <functional>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
+#include <istream>
 #include <queue>
-#include <set>
 #include <sstream>
-#include <stack>
+#include <stdexcept>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
-
-using namespace std;
-
-using ll = long long;
-
-enum class Term { kOld, kNumber };
-enum class OperationType { kAdd, kSubtract, kMultiply, kDivide };
-
-function<int(int old_worry_level)> ParseOperation(const string &line) {
-  string op = line.substr(line.find('=') + 2);
-  stringstream ss{op};
-  vector<string> tokens;
-  string token;
-  while (getline(ss, token, ' ')) {
-    tokens.push_back(token);
-  }
-
-  Term left_term = Term::kOld;
-  if (tokens[0] == "old") {
-    left_term = Term::kOld;
-  } else {
-    left_term = Term::kNumber;
-  }
-
-  Term right_term = Term::kOld;
-  if (tokens[2] == "old") {
-    right_term = Term::kOld;
-  } else {
-    right_term = Term::kNumber;
-  }
-
-  OperationType operation_type = OperationType::kAdd;
-
-  switch (tokens[1][0]) {
-  case '+':
-    operation_type = OperationType::kAdd;
-    break;
-  case '-':
-    operation_type = OperationType::kSubtract;
-    break;
-  case '*':
-    operation_type = OperationType::kMultiply;
-    break;
-  case '/':
-    operation_type = OperationType::kDivide;
-    break;
-  default:
-    assert(false);
-  }
-
-  return [=](int old_worry_level) -> int {
-    int left = 0;
-    int right = 0;
-    switch (left_term) {
-    case Term::kOld:
-      left = old_worry_level;
-      break;
-    case Term::kNumber:
-      left = stoi(tokens[0]);
-      break;
-    }
-    switch (right_term) {
-    case Term::kOld:
-      right = old_worry_level;
-      break;
-    case Term::kNumber:
-      right = stoi(tokens[2]);
-      break;
-    }
-
-    switch (operation_type) {
-    case OperationType::kAdd:
-      return left + right;
-    case OperationType::kSubtract:
-      return left - right;
-    case OperationType::kMultiply:
-      return left * right;
-    case OperationType::kDivide:
-      return left / right;
-    default:
-      assert(false);
-    }
-  };
-}
-
-function<bool(int new_worry_level)> ParseTest(const string &line) {
-  string test = line.substr(line.find(':') + 2);
-  stringstream ss{test};
-  vector<string> tokens;
-  string token;
-  while (getline(ss, token, ' ')) {
-    tokens.push_back(token);
-  }
-  if (tokens[0] == "divisible") {
-    int modulus = stoi(tokens[2]);
-    return [=](int new_worry_level) -> bool {
-      return new_worry_level % modulus == 0;
-    };
-  } else {
-    assert(false);
-  }
-}
-
-int ParseAction(const string &line) {
-  string action = line.substr(line.find(':') + 2);
-  stringstream ss{action};
-  vector<string> tokens;
-  string token;
-  while (getline(ss, token, ' ')) {
-    tokens.push_back(token);
-  }
-  int target = stoi(tokens.back());
-  return target;
-}
 
 struct Monkey {
-  int id = 0;
-  queue<int> items;
-  function<int(int old_worry_level)> operation;
-  function<bool(int new_worry_level)> test;
-  int on_true_target = 0;
-  int on_false_target = 0;
+  std::queue<std::int32_t> items{};
+  std::function<std::int32_t(std::int32_t)> inspection_operation{};
+  std::function<std::size_t(std::int32_t)> check_operation{};
+
+  [[nodiscard]] static Monkey Parse(const std::vector<std::string>& lines) {
+    if (lines.size() < 6) {
+      throw std::runtime_error{"Invalid monkey"};
+    }
+    Monkey monkey{};
+    monkey.items = ParseItems(lines[1]);
+    monkey.inspection_operation = ParseInspectionOperation(lines[2]);
+    monkey.check_operation = ParseCheckOperation(lines[3], lines[4], lines[5]);
+    return monkey;
+  }
+
+ private:
+  [[nodiscard]] static std::queue<std::int32_t> ParseItems(std::string_view s) {
+    const auto colon_position = s.find(':');
+    if (colon_position == std::string_view::npos) {
+      throw std::runtime_error{"Invalid items"};
+    }
+    std::queue<std::int32_t> items{};
+    std::istringstream iss{std::string{s.substr(colon_position + 1)}};
+    std::string item{};
+    while (std::getline(iss, item, ',')) {
+      items.push(std::stoi(item));
+    }
+    return items;
+  }
+
+  [[nodiscard]] static std::function<std::int32_t(std::int32_t)> ParseInspectionOperation(std::string_view s) {
+    const auto old_position = s.find("old");
+    if (old_position == std::string_view::npos) {
+      throw std::runtime_error{"Invalid inspection operation"};
+    }
+    const char operation = s[old_position + 4];
+    const std::optional<std::int32_t> operand = [&s, old_position]() -> std::optional<std::int32_t> {
+      try {
+        return std::stoi(std::string{s.substr(old_position + 5)});
+      } catch (...) {
+        return std::nullopt;
+      }
+    }();
+    return [operation, operand](std::int32_t value) {
+      switch (operation) {
+        case '+':
+          return value + operand.value_or(value);
+        case '-':
+          return value - operand.value_or(value);
+        case '*':
+          return value * operand.value_or(value);
+        case '/':
+          return value / operand.value_or(value);
+        default:
+          throw std::runtime_error{"Invalid inspection operation"};
+      }
+    };
+  }
+
+  [[nodiscard]] static std::function<std::size_t(std::int32_t)> ParseCheckOperation(std::string_view s1,
+                                                                                    std::string_view s2,
+                                                                                    std::string_view s3) {
+    const auto divisible_by_location = s1.find("divisible by");
+    if (divisible_by_location == std::string_view::npos) {
+      throw std::runtime_error{"Invalid check operation"};
+    }
+    const auto divider = std::stoi(std::string{s1.substr(divisible_by_location + 13)});
+    const auto true_monkey_position = s2.find("monkey");
+    if (true_monkey_position == std::string_view::npos) {
+      throw std::runtime_error{"Invalid check operation"};
+    }
+    const auto true_monkey = std::stoi(std::string{s2.substr(true_monkey_position + 6)});
+    const auto false_monkey_position = s3.find("monkey");
+    if (false_monkey_position == std::string_view::npos) {
+      throw std::runtime_error{"Invalid check operation"};
+    }
+    const auto false_monkey = std::stoi(std::string{s3.substr(false_monkey_position + 6)});
+    return [divider, true_monkey, false_monkey](std::int32_t value) {
+      return value % divider == 0 ? true_monkey : false_monkey;
+    };
+  }
 };
 
-Monkey ParseMonkey(const vector<string> &lines) {
-  static int id = 0;
-  Monkey monkey;
-  monkey.id = id++;
-
-  auto items = lines[1].substr(lines[1].find(':') + 1);
-  stringstream ss(items);
-  string item;
-  while (getline(ss, item, ',')) {
-    monkey.items.push(stoi(item));
-  }
-
-  monkey.operation = ParseOperation(lines[2]);
-  monkey.test = ParseTest(lines[3]);
-  monkey.on_true_target = ParseAction(lines[4]);
-  monkey.on_false_target = ParseAction(lines[5]);
-  return monkey;
-}
-
-vector<Monkey> ReadInput() {
-  vector<Monkey> monkeys;
-  string line;
-  int empty_line_count = 0;
-  vector<string> monkey_lines;
-  while (getline(cin, line)) {
+int main() {
+  std::vector<std::string> monkey_lines{};
+  std::string line{};
+  std::vector<Monkey> monkeys{};
+  std::size_t empty_line_count{0};
+  while (std::getline(std::cin, line)) {
     if (line.empty()) {
-      ++empty_line_count;
-      if (empty_line_count >= 2)
-        break;
-      else
-        monkeys.push_back(ParseMonkey(monkey_lines));
+      if (empty_line_count == 0) {
+        monkeys.push_back(Monkey::Parse(monkey_lines));
+      }
       monkey_lines.clear();
+      ++empty_line_count;
+      if (empty_line_count == 2) {
+        break;
+      }
     } else {
-      empty_line_count = 0;
       monkey_lines.push_back(line);
+      empty_line_count = 0;
     }
   }
-  return monkeys;
-}
 
-int Solve() {
-  auto monkeys = ReadInput();
-
-  vector<int> inspections(monkeys.size(), 0);
-  for (int r = 1; r <= 20; ++r) {
-    for (auto &monkey : monkeys) {
+  std::vector<std::size_t> inspection_counts(monkeys.size(), 0);
+  for (std::size_t r = 1; r <= 20; ++r) {
+    for (std::size_t monkey_index = 0; monkey_index < monkeys.size(); ++monkey_index) {
+      Monkey& monkey = monkeys[monkey_index];
       while (!monkey.items.empty()) {
-        ++inspections[monkey.id];
-        int old_worry_level = monkey.items.front();
+        std::int32_t worry_level = monkey.items.front();
         monkey.items.pop();
-        int new_worry_level = monkey.operation(old_worry_level);
-        new_worry_level /= 3;
-        if (monkey.test(new_worry_level)) {
-          monkeys[monkey.on_true_target].items.push(new_worry_level);
-        } else {
-          monkeys[monkey.on_false_target].items.push(new_worry_level);
-        }
+        worry_level = monkey.inspection_operation(worry_level);
+        worry_level /= 3;
+        const std::size_t target_monkey_index = monkey.check_operation(worry_level);
+        ++inspection_counts[monkey_index];
+        monkeys[target_monkey_index].items.push(worry_level);
       }
     }
   }
-  sort(inspections.begin(), inspections.end());
-  return inspections[inspections.size() - 1] *
-         inspections[inspections.size() - 2];
-}
 
-int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  std::sort(inspection_counts.begin(), inspection_counts.end());
+  std::cout << inspection_counts[inspection_counts.size() - 1] * inspection_counts[inspection_counts.size() - 2]
+            << '\n';
 }
