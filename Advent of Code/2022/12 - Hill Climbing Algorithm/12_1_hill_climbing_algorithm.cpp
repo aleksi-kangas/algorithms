0f1d252279122111a437476d5f87accd802cafbb
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-12-02 14:52:52 +0200

    Advent of Code | 2022 | 12 Hill Climbing Algorithm | Part 1

--- Advent of Code/2022/12 - Hill Climbing Algorithm/12_1_hill_climbing_algorithm.cpp
+++ Advent of Code/2022/12 - Hill Climbing Algorithm/12_1_hill_climbing_algorithm.cpp
@@ -1,122 +1,116 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <functional>
+#include <cstdint>
 #include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <optional>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <ranges>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
-using namespace std;
+struct Graph {
+  struct Vertex {
+    std::pair<std::size_t, std::size_t> position{};
+    char id{'a'};
+    std::int32_t elevation{0};
+    std::size_t steps{0};
+    std::vector<Vertex*> neighbors{};
+  };
 
-using ll = long long;
+  std::vector<std::vector<Vertex>> grid{};
+  std::pair<std::size_t, std::size_t> start_position{};
+  std::pair<std::size_t, std::size_t> end_position{};
 
-vector<vector<char>> ReadGrid(pair<int, int> &start, pair<int, int> &end) {
-  vector<vector<char>> grid;
-  string line;
-  while (getline(cin, line) && !line.empty()) {
-    vector<char> row;
-    for (char c : line) {
-      if (c == 'S') {
-        start = {grid.size(), row.size()};
-        row.push_back('a');
-      } else if (c == 'E') {
-        end = {grid.size(), row.size()};
-        row.push_back('z');
-      } else {
-        row.push_back(c);
+  [[nodiscard]] static Graph Parse(std::istream& is) {
+    Graph graph{};
+    std::string line{};
+    std::size_t row{0};
+    while (std::getline(is, line) && !line.empty()) {
+      graph.grid.push_back(ParseLine(line, row++));
+    }
+    graph.ConnectNeighbors();
+    graph.FindStartAndEndPositions();
+    return graph;
+  }
+
+  [[nodiscard]] std::size_t MinimumStepsBFS() {
+    std::ranges::for_each(grid, [](std::vector<Vertex>& row_vertices) {
+      std::ranges::for_each(row_vertices, [](Vertex& vertex) { vertex.steps = 0; });
+    });
+    std::vector<std::vector<bool>> visited(grid.size(), std::vector<bool>(grid[0].size(), false));
+    std::queue<Vertex*> q{};
+    q.push(&grid[start_position.first][start_position.second]);
+    visited[start_position.first][start_position.second] = true;
+    while (!q.empty()) {
+      const auto vertex = q.front();
+      q.pop();
+      for (const auto& neighbor : vertex->neighbors) {
+        if (!visited[neighbor->position.first][neighbor->position.second]) {
+          neighbor->steps = vertex->steps + 1;
+          visited[neighbor->position.first][neighbor->position.second] = true;
+          q.push(neighbor);
+        }
       }
     }
-    grid.push_back(row);
+    return grid[end_position.first][end_position.second].steps;
   }
-  return grid;
-}
 
-struct Node {
-  vector<shared_ptr<Node>> neighbors;
-  int row = 0;
-  int column = 0;
-  int steps = 0;
+ private:
+  [[nodiscard]] static std::vector<Vertex> ParseLine(std::string_view line, std::size_t row) {
+    std::vector<Vertex> vertices{};
+    std::size_t column{0};
+    for (const char c : line) {
+      std::int32_t elevation{0};
+      switch (c) {
+        case 'S':
+          elevation = 'a' - 'a';
+          break;
+        case 'E':
+          elevation = 'z' - 'a';
+          break;
+        default:
+          elevation = c - 'a';
+          break;
+      }
+      vertices.push_back(Vertex{.position = {row, column++}, .id = c, .elevation = elevation});
+    }
+    return vertices;
+  }
+
+  void ConnectNeighbors() {
+    std::ranges::for_each(grid, [](std::vector<Vertex>& row_vertices) {
+      std::ranges::for_each(row_vertices, [](Vertex& vertex) { vertex.neighbors.clear(); });
+    });
+    for (std::size_t r = 0; r < grid.size(); ++r) {
+      for (std::size_t c = 0; c < grid[0].size(); ++c) {
+        if (const bool up = r > 0 && grid[r][c].elevation - grid[r - 1][c].elevation >= -1) {
+          grid[r][c].neighbors.push_back(&grid[r - 1][c]);
+        }
+        if (const bool down = r < grid.size() - 1 && grid[r][c].elevation - grid[r + 1][c].elevation >= -1) {
+          grid[r][c].neighbors.push_back(&grid[r + 1][c]);
+        }
+        if (const bool left = c > 0 && grid[r][c].elevation - grid[r][c - 1].elevation >= -1) {
+          grid[r][c].neighbors.push_back(&grid[r][c - 1]);
+        }
+        if (const bool right = c < grid[0].size() - 1 && grid[r][c].elevation - grid[r][c + 1].elevation >= -1) {
+          grid[r][c].neighbors.push_back(&grid[r][c + 1]);
+        }
+      }
+    }
+  }
+
+  void FindStartAndEndPositions() {
+    for (std::size_t r = 0; r < grid.size(); ++r) {
+      for (std::size_t c = 0; c < grid[0].size(); ++c) {
+        if (grid[r][c].id == 'S') {
+          start_position = {r, c};
+        } else if (grid[r][c].id == 'E') {
+          end_position = {r, c};
+        }
+      }
+    }
+  }
 };
 
-vector<vector<shared_ptr<Node>>>
-BuildGridGraph(const vector<vector<char>> &grid) {
-  int rows = static_cast<int>(grid.size());
-  int cols = static_cast<int>(grid[0].size());
-  vector<vector<shared_ptr<Node>>> graph(rows, vector<shared_ptr<Node>>(cols));
-  for (int r = 0; r < rows; ++r) {
-    for (int c = 0; c < cols; ++c) {
-      graph[r][c] = make_shared<Node>();
-      graph[r][c]->row = r;
-      graph[r][c]->column = c;
-    }
-  }
-
-  for (int r = 0; r < rows; ++r) {
-    for (int c = 0; c < cols; ++c) {
-      if (0 < r && grid[r][c] - grid[r - 1][c] >= -1) {
-        graph[r][c]->neighbors.push_back(graph[r - 1][c]);
-      }
-      if (r < rows - 1 && grid[r][c] - grid[r + 1][c] >= -1) {
-        graph[r][c]->neighbors.push_back(graph[r + 1][c]);
-      }
-      if (0 < c && grid[r][c] - grid[r][c - 1] >= -1) {
-        graph[r][c]->neighbors.push_back(graph[r][c - 1]);
-      }
-      if (c < cols - 1 && grid[r][c] - grid[r][c + 1] >= -1) {
-        graph[r][c]->neighbors.push_back(graph[r][c + 1]);
-      }
-    }
-  }
-  return graph;
-}
-
-void BFS(vector<vector<shared_ptr<Node>>> &graph, const pair<int, int> &start) {
-  vector<vector<bool>> visited(graph.size(),
-                               vector<bool>(graph[0].size(), false));
-
-  queue<shared_ptr<Node>> q;
-  q.push(graph[start.first][start.second]);
-  visited[start.first][start.second] = true;
-
-  while (!q.empty()) {
-    auto node = q.front();
-    q.pop();
-    for (const auto &neighbor : node->neighbors) {
-      if (!visited[neighbor->row][neighbor->column]) {
-        visited[neighbor->row][neighbor->column] = true;
-        neighbor->steps = node->steps + 1;
-        q.push(neighbor);
-      }
-    }
-  }
-}
-
-int Solve() {
-  pair<int, int> start, end;
-  const auto grid = ReadGrid(start, end);
-  auto graph = BuildGridGraph(grid);
-
-  BFS(graph, start);
-  return graph[end.first][end.second]->steps;
-}
-
 int main() {
-  auto answer = Solve();
-  cout << answer << endl;
+  auto graph = Graph::Parse(std::cin);
+  std::cout << graph.MinimumStepsBFS() << std::endl;
 }
