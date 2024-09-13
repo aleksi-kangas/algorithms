5addd6c38c80e869b0010e0b5a5da37740def9ae
Aleksi Kangas <aleksi.kangas@hotmail.com>
2024-09-13 17:02:49 +0300

    Advent of Code | 2020 | 03 Toboggan Trajectory | Part 1

---
+++ Advent of Code/2020/03 - Toboggan Trajectory/Part 1/Solution.java
@@ -0,0 +1,54 @@
+import java.io.InputStream;
+import java.util.*;
+
+class Solution {
+
+  static final class Map {
+
+    private final Set<Pair<Integer, Integer>> trees = new HashSet<>();
+    private final Pair<Integer, Integer> size;
+
+    Map(final InputStream inputStream) {
+      final Scanner scanner = new Scanner(inputStream);
+      String s = scanner.nextLine();
+      int x = 0;
+      int y = 0;
+      while (!s.isEmpty()) {
+        for (int i = 0; i < s.length(); ++i) {
+          if (s.charAt(i) == '#') {
+            trees.add(new Pair<>(i, y));
+          }
+        }
+        x = Math.max(x, s.length());
+        ++y;
+        s = scanner.nextLine();
+      }
+      size = new Pair<>(x, y);
+    }
+
+    int CountTrees(final Pair<Integer, Integer> slope) {
+      int count = 0;
+      int x = 0;
+      int y = 0;
+      while (y <= size.v2) {
+        if (trees.contains(new Pair<>(x, y))) {
+          ++count;
+        }
+        x += slope.v1;
+        x %= size.v1;
+        y += slope.v2;
+      }
+      return count;
+    }
+  }
+
+  public static void main(String[] args) {
+    final Map map = new Map(System.in);
+    final int solution = map.CountTrees(new Pair<>(3, 1));
+    System.out.println(solution);
+  }
+
+  private record Pair<T1, T2>(T1 v1, T2 v2) {
+
+  }
+}
